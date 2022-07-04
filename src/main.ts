623c793a399543275e70fcf892a2cf7ec883dc50
Carlos Eduardo Hipólito <carlos.hipolito@Carloss-MacBook-Air.local>
2022-07-04 13:06:18 -0300

    port as a env variable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config();
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('server running on port 3000');
+  await app.listen(port);
+  console.log(`server running on port ${port}`);
 }
 
 bootstrap();
